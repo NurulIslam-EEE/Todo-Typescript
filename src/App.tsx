9efcfdd65e453362d5ca16329b0fbbe037a5ff7e
Nurul Islam <nurul.islam02310@gmail.com>
2022-05-02 14:54:05 +0600

    initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// let name: string;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
+// let printName2: Function;
+
+// let printName3: (name: string) => never;
+
+// let personName:unknown
+
+// function printName(name:string){
+
+// }
+// printName('x')
+
+// type Person={
+//   name:string;
+//   age?:number
+// }
+
+// let person:Person={
+//   name:'c'
+
+// }
+// let lotOfPeople:Person[];
+
+
+
+// interface Person {
+//   name: string;
+//   age?: number;
+// }
+
+// interface Guy extends Person{
+//   profession:string
+// }
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// type Y = X & {
+//   c: string;
+//   d: number;
+// };
+// let y: Y= {
+//   c:'ccc',
+//   d:42,
+//   a:'',
+//   b:8
+// };
+
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// interface Person extends X {
+//   name: string;
+//   age?: number;
+// }
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
